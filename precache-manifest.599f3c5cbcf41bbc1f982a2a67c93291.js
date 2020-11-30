d4b8ceab708c004d0c94b0dcd253d0de9640b1dc
RahulShahUI <38944466+RahulShahUI@users.noreply.github.com>
2020-11-30 12:53:01 +0530

    Add files via upload

---
+++ precache-manifest.599f3c5cbcf41bbc1f982a2a67c93291.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecd4927c75a2098c7332d8aa9cad7760",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2963d5c0f6bcc2744180",
+    "url": "/static/css/2.c4293ee4.chunk.css"
+  },
+  {
+    "revision": "b29ef9fe0c4b2544e806",
+    "url": "/static/css/main.0a33f0db.chunk.css"
+  },
+  {
+    "revision": "2963d5c0f6bcc2744180",
+    "url": "/static/js/2.caf7e598.chunk.js"
+  },
+  {
+    "revision": "21b12a9ccacf302a84f5cb80d9b0aae6",
+    "url": "/static/js/2.caf7e598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b29ef9fe0c4b2544e806",
+    "url": "/static/js/main.f737e0e3.chunk.js"
+  },
+  {
+    "revision": "ee755dc46d5fcd07cd10",
+    "url": "/static/js/runtime-main.9e305499.js"
+  }
+]);
